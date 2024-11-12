8b490b7d00a838853b749c8d1578ce94fe8123e2
rootgh <95168267+Aliko2020@users.noreply.github.com>
2024-11-11 15:14:52 -1200

    cors

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-const express = require('express');
-const cors = require('cors');
-const connectdb = require('.//config/db');
-const Router = require('./routes/feedRoute');
-require('dotenv').config();
-
+const express = require("express");
+const cors = require("cors");
+const connectdb = require(".//config/db");
+const Router = require("./routes/feedRoute");
+require("dotenv").config();
 
 const Port = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5174",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.urlencoded({ extended: true }));
 
-
-
-app.use('/', Router);
-
+app.use("/", Router);
 
 app.listen(Port, () => {
   console.log(`Server running on port: ${Port}`);
-  connectdb()
+  connectdb();
 });
